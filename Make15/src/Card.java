1e0f2faf5eab265f76d258242d2acf94eaf25a03
Abbyrea2002 <abbyrea2002@gmail.com>
2024-12-09 14:52:13 +0000

    push

---
+++ Make15/src/Card.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+/**
+ * Created by abbyr on 07/11/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class Card implements Comparable<Card>
+{
+   private final int RANK, SUIT;
+
+
+
+
+   private static final Random GENERATOR = new Random();
+   private static final String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+   private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+   //private static final String[] COLOURS = {"Red", "Black"};
+
+   public Card(){
+      RANK = GENERATOR.nextInt(RANKS.length);
+      SUIT = GENERATOR.nextInt(SUITS.length);
+      //COLOUR = (SUIT == 1 || SUIT == 2) ? 0 : 1;
+
+
+   }
+   public Card(int r, int s){
+      RANK = r;
+      SUIT = s;
+   }
+
+   public String getRank(){
+      return RANKS[RANK];
+
+   }
+
+   public String getSuit(){
+      return SUITS[SUIT];
+
+   }
+
+   //public String getColour(){
+     // return COLOURS[COLOUR];
+   //fde QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ0-----PO0}
+
+   public int getRankValue(){
+      return RANK;
+   }
+
+
+
+
+
+   @Override
+   public String toString(){
+      return getRank() + " of " + getSuit();
+
+   }
+
+   @Override
+   public int compareTo(Card otherCard){
+      if(this.getRankValue() > otherCard.getRankValue()) return 1;
+      else if(this.getRankValue() < otherCard.getRankValue()) return -1;
+      else return 0;
+   }
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) return true; // Same reference
+      if (obj == null || getClass() != obj.getClass()) return false; // Type check
+
+      Card otherCard = (Card) obj; // Cast the object to Card
+      return this.getSuit().equals(otherCard.getSuit()) && this.getRank().equals(otherCard.getRank());
+   }
+}//class
---
+++ Make15/src/Deck.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+/**
+ * Created by abbyr on 12/11/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class Deck
+{
+   private static int NUMCARDS = 52;
+   private final Card[] CARDS = new Card[NUMCARDS];
+   private int cardIndex;
+
+   public Deck(){
+      for(int thisCard=0; thisCard < NUMCARDS; thisCard++){
+         this.CARDS[thisCard] = new Card(thisCard % 13, thisCard/13);
+      }
+      this.cardIndex = NUMCARDS - 1;
+      this.shuffle();
+   }
+
+   private void shuffle(){
+      int index;
+      Card temp;
+      Random random = new Random();
+      for(int i = this.CARDS.length - 1; i > 0; i--){
+         index = random.nextInt(i + 1);
+         temp = this.CARDS[index];
+         this.CARDS[index] = this.CARDS[i];
+         this.CARDS[i] = temp;
+
+      }
+   }
+
+   public Card deal(){
+      return this.CARDS[this.cardIndex--];
+   }
+
+
+}//class
