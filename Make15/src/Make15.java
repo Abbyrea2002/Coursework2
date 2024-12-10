44e21cddbf2acbefdf4bb6bee61212a9f99ceec2
Abbyrea2002 <abbyrea2002@gmail.com>
2024-12-10 17:43:49 +0000

    push

---
+++ Make15/src/Make15.java
@@ -0,0 +1,17 @@
+/**
+ * Created by abbyr on 10/12/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class Make15
+{
+   private static final int NUMCARDSP = 5;
+   private static final int NUMCARDSC = 1;
+   private Card[] player = new Card[NUMCARDSP];
+   private Card[] computer = new Card[NUMCARDSC];
+   private int score = 0;
+
+   public static void main(String[] args){
+
+   }
+
+}//class
