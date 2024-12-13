12a7213055165f73e47b92fb37630bd9de15689c
Abbyrea2002 <abbyrea2002@gmail.com>
2024-12-13 17:03:35 +0000

    push

--- Make15/src/Hand.java
+++ Make15/src/Hand.java
@@ -12,7 +12,7 @@ public class Hand {
    }
 
    public ArrayList<Card> getCards(){
-      return this.cards;
+      return new ArrayList<>(cards);
 
    }
 
--- Make15/src/Make15.java
+++ Make15/src/Make15.java
@@ -9,7 +9,7 @@ public class Make15
    private int score = 0;
 
    public static void main(String[] args){
-
+      Make15 game = new Make15();
       Scanner scanner = new Scanner(System.in);
 
 
@@ -23,12 +23,33 @@ public class Make15
       }
 
       while(true){
-         computer.addCard(deck.deal());
+         int choice = -1;
+
+         System.out.println("Your hand is:");
          for(int i = 0; i < player.getHandSize(); i++){
-            System.out.println("Your hand is: " + player.getCards().get(i));
+
+            System.out.println((i + 1) + ". " + player.getCards().get(i));
+         }
+         computer.addCard(deck.deal());
+         System.out.println("Computer card: " + computer.getCards());
+
+         System.out.println("Enter the number of the card you would like to play: ");
+
+
+
+         try{
+            choice = Integer.parseInt(scanner.nextLine());
+            if(choice < 1 || choice > player.getHandSize()){
+               throw new IllegalArgumentException("Invalid, please enter a valid card number");
+            }
+
+         }catch(Exception e){
+            System.out.println("Invalid input. Please enter a number between 1 and 5");
+            continue;
+
          }
 
-         System.out.println("Computer card: " + computer);
+
 
 
 
